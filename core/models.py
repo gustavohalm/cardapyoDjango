ee8aa039d5c071ffc786d5a71a9d6c8e2b11d689
gustavo <contato@gustavohalm.com>
2019-09-20 19:18:56 -0300

    Model Restaurant

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your models he
+
+class Restaurant( models.Model):
+    name = models.CharField(max_length=256)
+    state = models.CharField(max_length=32)
+    city = models.CharField(max_length=256)
+    neighborhood = models.CharField(max_length=256)
+    address = models.CharField(max_length=512)
+    number = models.CharField(max_length=12)
+    cep = models.CharField(max_length=8)
