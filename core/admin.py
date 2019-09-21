03b61855d1dc54e8507b242e65aa2424c2489aa8
gustavo <contato@gustavohalm.com>
2019-09-20 21:47:10 -0300

    Settings

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import Table, Restaurant, Category, Product
+from menu.models import  Order
 
 # Register your models here.
+
+admin.site.register(Table)
+admin.site.register(Restaurant)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Order)
