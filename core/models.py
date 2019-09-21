56d078f05cecbc923f6b4714e5288c7c9b18e79b
gustavo <contato@gustavohalm.com>
2019-09-20 21:35:41 -0300

    Models for Core and Menu app's

--- core/models.py
+++ core/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-# Create your models he
 
-class Restaurant( models.Model):
+class Restaurant(models.Model):
     name = models.CharField(max_length=256)
     cnpj = models.CharField(max_length=222)
     state = models.CharField(max_length=32)
@@ -12,3 +11,19 @@ class Restaurant( models.Model):
     number = models.CharField(max_length=12)
     cep = models.CharField(max_length=8)
     owner = models.ForeignKey('auth.User', related_name='restaurants', on_delete=models.CASCADE)
+
+
+class Table(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='tables')
+    client = models.CharField(max_length=256)
+
+
+class Category(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='categories')
+    name = models.CharField(max_length=128)
+
+
+class Product(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=256)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from core.models import  Table, Restaurant
 
-# Create your models here.
+
+class Order(models.Model):
+
+    restaurant = models.ForeignKey('core.Restaurant', related_name='restaurant_orders', on_delete=models.CASCADE)
+    table = models.ForeignKey('core.Table', related_name='tables_orders', on_delete=models.CASCADE)
+    products = models.ManyToManyField('core.Product', related_name='orders_product')
+    total = models.DecimalField(max_digits=8, decimal_places=2)
+    KITCHEN_CHOICES = (
+        ('cendente', 'pendente'),
+        ('cancelado', 'cancelado'),
+        ('pronto', 'pronto')
+    )
+    kitchen_status = models.CharField( max_length=32, choices=KITCHEN_CHOICES)
+
+    TABLE_CHOICES = (
+        ('cendente', 'pendente'),
+        ('cancelado', 'cancelado'),
+        ('pronto', 'pronto')
+    )
+    table_status = models.CharField(max_length=32, choices=TABLE_CHOICES)
+
