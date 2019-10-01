0c51dc2f614da1f00235d3a7a63e211c6e9863fe
gustavo <contato@gustavohalm.com>
2019-09-30 21:19:28 -0300

    Core ViewSets API's

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -11,3 +11,33 @@ class RestaurantViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class TableViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.TableSerializer
+
+    def get_queryset(self):
+        return models.Restaurant.objects.filter(restaurant=self.request.user.restaurants)
+
+    def perform_create(self, serializer):
+        serializer.save(restaurant = self.request.user.restaurants)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.CategorySerializer
+
+    def get_queryset(self):
+        return models.Category.objects.filter(restaurant=self.request.user.restaurants)
+
+    def perform_create(self, serializer):
+        serializer.save(restaurant=self.request.user.restaurants)
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.ProductSerializer
+
+    def get_queryset(self):
+        return models.Product.objects.filter(restaurant=self.request.restaurants)
+
+    def perform_create(self, serializer):
+        serializer.save(restaurant=self.request.user.restaurants)
